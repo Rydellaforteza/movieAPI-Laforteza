25395b86ad0fad36f2da6114fc79efc113277c5d
rydellaforteza18 <rydellaforteza18@gmail.com>
2025-11-19 17:01:18 +0800

    debugged

--- index.js
+++ index.js
@@ -2,42 +2,71 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const port = process.env.PORT || 4000;
+const port = 4000;
+
 const app = express();
 
-
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://movie-app-client-neon.vercel.app",   
-    "https://movieapp-laforteza.onrender.com"    
-  ],
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 
+app.use(cors());
 
-mongoose.connect(
-  "mongodb+srv://admin:admin1234@rydelanndb.4ukmbcc.mongodb.net/b581-Movie-App?retryWrites=true&w=majority&appName=RydelAnnDB"
-)
-  .then(() => console.log("Now connected to MongoDB Atlas."))
-  .catch(err => console.error("MongoDB connection error:", err));
+mongoose.connect("mongodb+srv://admin:admin1234@rydelanndb.4ukmbcc.mongodb.net/b581-Movie-App?retryWrites=true&w=majority&appName=RydelAnnDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
+mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas.'));
 
-const movieRoutes = require("./routes/movie");
 const userRoutes = require("./routes/user");
+const movieRoutes = require("./routes/movie");
 
-app.use("/movies", movieRoutes);
 app.use("/users", userRoutes);
+app.use("/movies", movieRoutes);
 
-if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`API is now online on port ${port}`);
-  });
+if(require.main === module){
+    app.listen(process.env.PORT || port, () => {
+        console.log(`API is now online on port ${ process.env.PORT || port }`)
+    });
 }
 
-module.exports = { app, mongoose };
+module.exports = {app,mongoose};
+
+
+
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+
+// const port = process.env.PORT || 4000;
+// const app = express();
+
+
+// app.use(cors());
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+
+// mongoose.connect(
+//   "mongodb+srv://admin:admin1234@rydelanndb.4ukmbcc.mongodb.net/b581-Movie-App?retryWrites=true&w=majority&appName=RydelAnnDB"
+// )
+//   .then(() => console.log("Now connected to MongoDB Atlas."))
+//   .catch(err => console.error("MongoDB connection error:", err));
+
+
+// const movieRoutes = require("./routes/movie");
+// const userRoutes = require("./routes/user");
+
+// app.use("/movies", movieRoutes);
+// app.use("/users", userRoutes);
+
+// if (require.main === module) {
+//   app.listen(port, () => {
+//     console.log(`API is now online on port ${port}`);
+//   });
+// }
+
+// module.exports = { app, mongoose };
