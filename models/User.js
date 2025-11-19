d37135b1b8ba1f7060e23c71df32d487a41e6db4
rydellaforteza18 <rydellaforteza18@gmail.com>
2025-11-19 14:51:26 +0800

    fixed schema

--- models/User.js
+++ models/User.js
@@ -1,25 +1,26 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 6
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false
-    }
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
   },
-  { timestamps: true }
-);
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
+});
 
 module.exports = mongoose.model("User", userSchema);
